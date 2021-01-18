196d0076f80b156fd70a3b6cfba41db10b68eea9
feiqipengchen <dunqipeng@qq.com>
2021-01-18 23:07:14 +0800

    熟悉面向对象编程可以简单的看看该图书管理

---
+++ 面向对象编程练习--简易图书管理/operation/DisplayOperation.java
@@ -0,0 +1,17 @@
+package operation;
+
+import book.Book;
+import book.BookList;
+
+
+public class DisplayOperation implements IOperation{
+
+    @Override
+    public void work(BookList bookList) {
+        System.out.println("展示图书");
+        for (int i = 0; i < bookList.getUsedSize(); i++) {
+            Book book = bookList.getBook(i);
+            System.out.println(book);
+        }
+    }
+}
